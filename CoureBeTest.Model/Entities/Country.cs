b94f95b0f3929078eddca99d0b33779b5e4830b0
Ifeanyi Okeke <okekeifeanyi20@yahoo.com>
2025-12-06 00:12:54 +0100

    folder structure designed

---
+++ CoureBeTest.Model/Entities/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace CoureBeTest.Model.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryCode { get; set; }
+        public string Name { get; set; }
+        public string CountryIso { get; set; }
+        public ICollection<CountryDetail>? CountryDetails { get; set; }
+    }
+}
---
+++ CoureBeTest.Model/Entities/CountryDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace CoureBeTest.Model.Entities
+{
+    public class CountryDetail
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string Operator { get; set; }
+        public string OperatorCode { get; set; }
+
+        public Country Country { get; set; }
+    }
+}

7585c5aa8f1fe68f2f1c52bef92de634236f8de1
Ifeanyi Okeke <okekeifeanyi20@yahoo.com>
2025-12-06 02:52:15 +0100

    Completed

--- CoureBeTest.Core/DTOs/CountryLookupRequestDto.cs
+++ CoureBeTest.Core/DTOs/CountryLookupRequestDto.cs
@@ -1,7 +1,10 @@
-﻿namespace CoureBeTest.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoureBeTest.API.DTOs
 {
     public class CountryLookupRequestDto
     {
+        [Required]
         public string PhoneNumber { get; set; } = string.Empty;
     }
 }
